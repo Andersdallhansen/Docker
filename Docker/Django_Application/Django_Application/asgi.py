fe9403aea999767a67e2145599016459bdf5aaf2
anders-dall-hansen <anders.dall-hansen@maersk.com>
2022-02-26 18:01:58 +0100

    some changes

--- Docker/Django_Application/Django_Application/asgi.py
+++ Docker/Django_Application/Django_Application/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_application project.
+ASGI config for Django_Application project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_application.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Application.settings')
 
 application = get_asgi_application()
